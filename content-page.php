0d8ac22075146e87655aeede7aef31144911cc5d
Mike Schober <mschober@cj.com>
2012-09-03 12:34:52 -0700

    removing comments from pages

--- content-page.php
+++ content-page.php
@@ -17,6 +17,7 @@
 		<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
+
 	<footer class="entry-meta">
 		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
